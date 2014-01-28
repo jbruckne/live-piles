2f3aec32934b42f777371ddb4c241a98e62df3fb
Sargun Vohra <sargunster@gmail.com>
2014-01-27 23:35:21 -0500

    Replaced Date and Time tiles with a DateTime tile

--- config.py
+++ config.py
@@ -1,7 +1,4 @@
 from tiles import *
 
-def addClock(framework, x, y, w, h, bgcolor, textcolor):
-    framework.tiles.append(ClockTile(x, y, w, h, bgcolor, textcolor))
-
-def addDate(framework, x, y, w, h, bgcolor, textcolor):
-    framework.tiles.append(DateTile(x, y, w, h, bgcolor, textcolor))
+def addDateTime(framework, x, y, w, h, bgcolor, textcolor, formatstring, size):
+    framework.tiles.append(DateTimeTile(x, y, w, h, bgcolor, textcolor, formatstring, size))
--- tiles.py
+++ tiles.py
@@ -2,20 +2,17 @@ import time
 import pygame
 from framework import *
 
-class ClockTile(BasicTile):
-    time = None;
-    textcolor = (0, 0, 0);
+class DateTimeTile(BasicTile):
+    time = None
+    textcolor = (0, 0, 0)
+    formatstring = ""
+    size = 1/5.0
     
-    def getFormatString(self):
-        return '%I:%M:%S%p';
-    
-    def getSizeScalar(self):
-        return 1 / 4.5;
-    
-    def __init__(self, posx, posy, width, height, bgcolor, textcolor):
+    def __init__(self, posx, posy, width, height, bgcolor, textcolor, formatstring, size):
         BasicTile.__init__(self, posx, posy, width, height, bgcolor)
         self.textcolor = textcolor
-        
+        self.formatstring = formatstring
+        self.size = size
         
     def draw(self, screen):
         BasicTile.draw(self, screen)
@@ -26,16 +23,11 @@ class ClockTile(BasicTile):
         w = self.getw()
         h = self.geth()
         
-        myfont = pygame.font.SysFont("", int(min(w, h*2) * self.getSizeScalar()))
-        label = myfont.render(time.strftime(self.getFormatString()), 1, self.textcolor)
+        myfont = pygame.font.SysFont("", int(min(w, h*2) * self.size))
+        label = myfont.render(time.strftime(self.formatstring), 1, self.textcolor)
         
         textx = x + 0.5 * (w - label.get_width())
         texty = y + 0.5 * (h - label.get_height())
         screen.blit(label, (textx, texty))
-
-class DateTile(ClockTile):
-    def getFormatString(self):
-        return '%a %b %d, %Y';
-    
-    def getSizeScalar(self):
-        return 1 / 5.5;
+        
+        
